bf22dcb8a300fdb785ec0d21362144384f764355
Pete <peterllombardo@gmail.com>
2023-11-17 05:42:37 -0500

    Initial code commits

---
+++ src/PlayingCardList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useAxios } from "./hooks";
+import PlayingCard from "./PlayingCard";
+import { formatCard } from "./helpers";
+import "./PlayingCardList.css";
+
+/* Renders a list of playing cards.
+ * Can also add a new card at random. */
+function PlayingCardList() {
+  const [cards, addCard, clearCards] = useAxios(
+    "cards",
+    "https://deckofcardsapi.com/api/deck/new/draw/"
+  );
+  return (
+    <div className="PlayingCardList">
+      <h3>Pick a card, any card!</h3>
+      <div>
+        <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
+        <button onClick={clearCards}>Clear Cards!</button>
+      </div>
+      <div className="PlayingCardList-card-area">
+        {cards.map(cardData => (
+          <PlayingCard key={cardData.id} front={cardData.image} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+PlayingCardList.defaultProps = {};
+
+export default PlayingCardList;
